7b75b3ab9fffea94b88cd948b97f1f21237ec5a3
vanthinh.to@studenti.unimi.it <vanthinh.to@utenti.unimi.it@sdid306-005-3.linux.unicloud.unimi.it>
2026-03-19 15:43:07 +0100

    update

---
+++ program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  int value=5; 
+  printf("%d\n", value);
+  value=10;
+  printf("%d\n", value);
+  int length = 2;
+  int height=5;
+  int area=length*height;
+  printf("%d\n",area);
+  return 0;
+}
